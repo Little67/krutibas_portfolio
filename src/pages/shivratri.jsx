47df92917d40545510335e04871195b23bc7da6d
Satyanarayan Dalei <satyanarayandalei65@gmail.com>
2024-03-06 21:41:19 +0530

    Added

--- src/pages/shivratri.jsx
+++ src/pages/shivratri.jsx
@@ -22,14 +22,51 @@ const Shivratri = () => {
             link: "https://www.instagram.com/krutibasdwibedi",
         },
         {
+            name: "Alok",
+            image: "https://firebasestorage.googleapis.com/v0/b/myproject-b43c8.appspot.com/o/krutibas_website%2FWhatsApp%20Image%202024-03-06%20at%201.43.47%20PM.jpeg?alt=media&token=6b2a82e6-48ab-4162-a37b-cce9cec00b1f",
+            title: "",
+            link: "",
+        },
+        {
             name: "Satyanarayan Dalei",
             image: "https://media.licdn.com/dms/image/C4D03AQHbumNxTiwY9A/profile-displayphoto-shrink_400_400/0/1626777692393?e=1715212800&v=beta&t=FnTk7ATHsKlwOZFOBYjgMDe3uOVDCY5wowZGmHTW8as",
             title: "",
             link: "https://www.linkedin.com/in/satyadalei/",
         },
+        {
+            name: "Abinash Kaler",
+            image: "https://firebasestorage.googleapis.com/v0/b/myproject-b43c8.appspot.com/o/krutibas_website%2Fbb1c34e5-2370-4fc1-a1e8-34f762d2ee3a.jpg?alt=media&token=a8b8aa5b-93dc-4fbd-b05b-ac405641dd4c",
+            title: "",
+            link: "https://www.instagram.com/abinash_kaler?igsh=MXh6ZGx3MzVvN2Q4bQ==",
+        },
+        {
+            name: "Nityanarayan Nanda",
+            image: "https://firebasestorage.googleapis.com/v0/b/myproject-b43c8.appspot.com/o/krutibas_website%2FNityanarayan%20Nanda.jpg?alt=media&token=7dffc465-b077-4aff-9631-42a5719930cb",
+            title: "",
+            link: "",
+        },
     ]
 
-    const volunteers = []
+    const volunteers = [
+        {
+            name: "Anup",
+            image: "https://firebasestorage.googleapis.com/v0/b/myproject-b43c8.appspot.com/o/krutibas_website%2FAnup.jpg?alt=media&token=068094eb-20ba-4009-a9f6-1967a379bcfc",
+            title: "",
+            link: "",
+        },
+        {
+            name: "Rudra Partap",
+            image: "https://firebasestorage.googleapis.com/v0/b/myproject-b43c8.appspot.com/o/krutibas_website%2FRudra%20Partap.jpg?alt=media&token=8f5f3497-7fb5-4863-bd9d-c435b08dbd5e",
+            title: "",
+            link: "https://www.instagram.com/_.rudra_p._?utm_source=qr&igsh=ZWR0NzZ0aXRxYTIz",
+        },
+        {
+            name: "Pritam",
+            image: "https://firebasestorage.googleapis.com/v0/b/myproject-b43c8.appspot.com/o/krutibas_website%2Fpritam.jpg?alt=media&token=0b8a879e-117b-4875-a61e-20c3321f3708",
+            title: "",
+            link: "",
+        },
+    ]
 
     return (
         <React.Fragment>
@@ -56,8 +93,8 @@ const Shivratri = () => {
                         {/* --- ABOUT SECTION --- */}
                         <div className='flex flex-col md:flex-row min-h-72 justify-between' >
                             <p className='leading-relaxed md:w-[49%] mb-5 md:mr-3 md:leading-8 text-lg text-justify' >
-                                <span className='text-sky-500 font-bold' >Happy Maha Shivatri! </span> 
-                                 We are happy to invite you to the distribution of dahi pana on the eve of holy Maha Shivratri at IGIT temple. This is not an official event rather some of the students are organizing it. We have planned to provide nearly 800 - 1000 glass of dahi panas. We will auspiciously wait to your presence.  
+                                <span className='text-sky-500 font-bold' >Happy Maha Shivatri! </span>
+                                We are happy to invite you to the distribution of dahi pana on the eve of holy Maha Shivratri at IGIT temple. This is not an official event rather some of the students are organizing it. We have planned to provide nearly 800 - 1000 glass of dahi panas. We will auspiciously wait to your presence.
                                 <br />
                                 <br />
                                 {/* If any one left please do not mind. */}
@@ -84,7 +121,7 @@ const Shivratri = () => {
                         {/* --- EVENT DETAILS --- */}
                         <h1 className='title text-2xl mt-5 mb-3 md:mb-5 w-full' >Event details</h1>
                         <div className='flex md:flex-row min-h-96 justify-between flex-col-reverse ' >
-                            <div className="md:w-[45%] rounded-sm h-80 md:h-auto"
+                            <div className="md:w-[50%] rounded-sm h-80 md:h-auto"
                                 style={{
                                     backgroundImage: `url('../invitationa_maha_shiva_ratri.jpg')`,
                                     backgroundSize: 'cover',
@@ -93,7 +130,7 @@ const Shivratri = () => {
                                 }}
                             >
                             </div>
-                            <div className='leading-relaxed md:w-[49%] mb-5 md:mr-3 md:leading-8 text-lg text-justify' >
+                            <div className='leading-relaxed md:w-[40%] mb-5 md:mr-3 md:leading-8 text-lg text-justify' >
                                 <ul>
                                     <li>Date : 8th March 2024</li>
                                     <li>Time : 09:00 AM</li>
@@ -109,9 +146,19 @@ const Shivratri = () => {
                             }} >Coordinators</h1>
                             <div className='flex flex-wrap' >
                                 {coordinators.map((coordinator, index) => {
-                                    return (<a key={index} href={coordinator.link} target='_blank' rel="noreferrer" className='w-16 h-16 bg-red-400 rounded-full m-1 flex justify-center items-center overflow-hidden cursor-pointer' >
-                                        <img src={coordinator.image} alt="profile"  />
-                                    </a>)
+                                    return (<div className='mr-0.5' key={index} >
+                                        {coordinator.link !== "" ?
+                                            <a href={coordinator.link} target='_blank' rel="noreferrer" className='w-16 h-16 bg-gray-400 rounded-full m-1 flex justify-center items-center overflow-hidden cursor-pointer' >
+                                                <img src={coordinator.image} alt="profile" />
+                                            </a>
+                                            :
+                                            <div key={index}
+                                                className='w-16 h-16 bg-gray-400 rounded-full m-1 flex justify-center items-center overflow-hidden cursor-pointer' >
+                                                <img src={coordinator.image} alt="profile" />
+                                            </div>
+                                        }
+                                        <p className='text-sm text-center text-gray-500/70' >{coordinator.name.split(" ")[0]}</p>
+                                    </div>)
                                 })}
                             </div>
 
@@ -119,9 +166,20 @@ const Shivratri = () => {
                                 fontSize: "40px",
                                 fontWeight: "900",
                             }} >Volunteers</h1>
-                             <div className='flex flex-wrap' >
-                                {volunteers.map((volunteer) => {
-                                    return (<div className='w-16 h-16 bg-red-400 rounded-full m-1' >
+                            <div className='flex flex-wrap' >
+                                {volunteers.map((coordinator, index) => {
+                                    return (<div className='mr-0.5' key={index} >
+                                        {coordinator.link !== "" ?
+                                            <a href={coordinator.link} target='_blank' rel="noreferrer" className='w-16 h-16 bg-gray-400 rounded-full m-1 flex justify-center items-center overflow-hidden cursor-pointer' >
+                                                <img src={coordinator.image} alt="profile" />
+                                            </a>
+                                            :
+                                            <div key={index}
+                                                className='w-16 h-16 bg-gray-400 rounded-full m-1 flex justify-center items-center overflow-hidden cursor-pointer' >
+                                                <img src={coordinator.image} alt="profile" />
+                                            </div>
+                                        }
+                                        <p className='text-sm text-center text-gray-500/70' >{coordinator.name.split(" ")[0]}</p>
                                     </div>)
                                 })}
                             </div>
@@ -129,7 +187,7 @@ const Shivratri = () => {
                         </div>
                     </div>
                     <div className="page-footer">
-                       <Footer />
+                        <Footer />
                     </div>
                 </div>
             </div>
